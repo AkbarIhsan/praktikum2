72e150de71de7775f66e89c4c9734543d3e8e74f
Amaliastf <125347989+Amaliastf@users.noreply.github.com>
2023-03-07 14:08:15 +0700

    ikimaneh

--- resources/views/amal.blade.php
+++ resources/views/amal.blade.php
@@ -6,13 +6,13 @@
     <div class="card" style="width: 18rem;">
     <img src=" {{asset('img/amal.jpg')}}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-text"><center>أماليا سلسا لطفيانا</center></h5>
-      <p class="card-text">أهلا</p>
+      <h5 class="card-text"><center>Amalia Salsa Lutfiana</center></h5>
+      <p class="card-text">oit</p>
     </div>
     <ul class="list-group list-group-flush">
-      <li class="list-group-item">المرأة</li>
-      <li class="list-group-item">كراسيك</li>
-      <li class="list-group-item">أحبّك في الله</li>
+      <li class="list-group-item">Female</li>
+      <li class="list-group-item">Gresik</li>
+      <li class="list-group-item">Iyaes</li>
     </ul>
   </div>
 </center>
